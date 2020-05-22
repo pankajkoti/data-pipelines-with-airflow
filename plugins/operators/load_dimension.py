3236be17a04e7604e0eb1ebb53c216965595fcfa
pankaj.koti <pankaj.koti@olx.com>
2020-05-22 23:44:38 +0200

    Add custom operator to load data in fact and dimensional tables from staging tables within Redshift

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,21 +2,41 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadDimensionOperator(BaseOperator):
+from helpers import SqlQueries
 
+
+class LoadDimensionOperator(BaseOperator):
     ui_color = '#80BD9E'
+
+    truncate_query = """
+            TRUNCATE TABLE {table}
+        """
+    insert_query = """
+            INSERT INTO {table} 
+            {select_query}
+        """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_query_name="",
+                 truncate_table=True,
                  *args, **kwargs):
-
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query_name = sql_query_name
+        self.truncate_table = truncate_table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(f'LoadDimensionOperator in progress for table: {self.table}')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate_table:
+            self.log.info(f"Truncating dimension table {self.table}")
+            redshift.run(LoadDimensionOperator.truncate_query.format(
+                table=self.table))
+        self.log.info(f"Inserting data into dimension table: {self.table}")
+        select_sql_query = getattr(SqlQueries, self.sql_query_name)
+        redshift.run(LoadDimensionOperator.insert_query.format(
+            table=self.table, select_query=select_sql_query))
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,21 +2,41 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadFactOperator(BaseOperator):
+from helpers import SqlQueries
 
+
+class LoadFactOperator(BaseOperator):
     ui_color = '#F98866'
+
+    truncate_query = """
+        TRUNCATE TABLE {table}
+    """
+    insert_query = """
+        INSERT INTO {table} 
+        {select_query}
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_query_name="",
+                 truncate_table=False,
                  *args, **kwargs):
-
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query_name = sql_query_name
+        self.truncate_table = truncate_table
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        self.log.info(f'LoadFactOperator in progress for table: {self.table}')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if self.truncate_table:
+            self.log.info(f"Truncating fact table: {self.table}")
+            redshift.run(LoadFactOperator.truncate_query.format(
+                table=self.table))
+        self.log.info(f"Inserting data into fact table {self.table}")
+        select_sql_query = getattr(SqlQueries, self.sql_query_name)
+        redshift.run(LoadFactOperator.insert_query.format(
+            table=self.table, select_query=select_sql_query))

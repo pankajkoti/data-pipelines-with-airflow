9ed261a2e4985daf32f798011933b9ece3a2ff25
pankaj.koti <pankaj.koti@olx.com>
2020-05-22 23:42:53 +0200

    Fix create tables to create time dimensional table

--- create_tables.sql
+++ create_tables.sql
@@ -62,17 +62,14 @@ CREATE TABLE public.staging_songs (
 	"year" int4
 );
 
-CREATE TABLE public.staging_songs (
-	num_songs int4,
-	artist_id varchar(256),
-	artist_name varchar(256),
-	artist_latitude numeric(18,0),
-	artist_longitude numeric(18,0),
-	artist_location varchar(256),
-	song_id varchar(256),
-	title varchar(256),
-	duration numeric(18,0),
-	"year" int4
+CREATE TABLE IF NOT EXISTS public.time(
+    start_time TIMESTAMP PRIMARY KEY,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER
 );
 
 CREATE TABLE public.users (
@@ -83,8 +80,3 @@ CREATE TABLE public.users (
 	"level" varchar(256),
 	CONSTRAINT users_pkey PRIMARY KEY (userid)
 );
-
-
-
-
-
